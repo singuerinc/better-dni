fd7686a15d1dfd2a462f00c135ac63cef6155329
singuerinc <nahuel.scotti@gmail.com>
2018-05-18 22:09:03 +0200

    feat: add typescript def

---
+++ types/index.d.ts
@@ -0,0 +1,13 @@
+/**
+ * Returns true if the string is a NIE
+ */
+declare const isNIE: (value: string) => boolean;
+/**
+ * Returns true if the string is a NIF
+ */
+declare const isNIF: (value: string) => boolean;
+/**
+ * Returns true if the string is a valid DNI
+ */
+declare const isValid: (value: string) => boolean;
+export { isValid, isNIE, isNIF };
