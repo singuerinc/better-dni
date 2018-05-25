fff039d7c58d7be801f7c2983294f3bc1e43c335
singuerinc <nahuel.scotti@gmail.com>
2018-05-25 22:27:21 +0200

    docs: add "@since"

--- src/isNIE.js
+++ src/isNIE.js
@@ -5,6 +5,7 @@ import { ctrlChar } from './ctrlChar';
  * Returns true if the string is a NIE
  * @param {string} value
  * @returns {boolean}
+ * @since 1.0.1
  * @example
  * isNIE("X4108613P"); // => true
  */
